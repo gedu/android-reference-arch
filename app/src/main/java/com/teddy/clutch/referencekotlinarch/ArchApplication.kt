33c65d696ddde4b5b4459ccbd173a86042af9b41
teddy <gedu07@gmail.com>
2018-10-31 14:12:04 -0300

    Warning

--- app/src/main/java/com/teddy/clutch/referencekotlinarch/ArchApplication.kt
+++ app/src/main/java/com/teddy/clutch/referencekotlinarch/ArchApplication.kt
@@ -5,6 +5,7 @@ import com.teddy.clutch.referencekotlinarch.utils.di.appModule
 import com.teddy.clutch.referencekotlinarch.utils.di.networkModule
 import org.koin.android.ext.android.startKoin
 
+@Suppress("unused")// it is in the Android Manifest
 class ArchApplication : Application() {
 
   override fun onCreate() {
